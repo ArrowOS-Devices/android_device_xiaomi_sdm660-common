c6e4969f1c2c45115b9faab70719f2aad5af6559
TH779 <tongsj2333@gmail.com>
2020-02-07 16:24:17 +0000

    sdm660-common: Decommonize vibrator HAL
     * Clover don't have it.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -438,11 +438,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm660
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-ext \
