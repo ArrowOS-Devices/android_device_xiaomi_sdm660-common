6ea5af5e6d6cac7a741780c8a84ff380acae4d3e
Ratoriku <a1063021545@gmail.com>
2021-10-30 21:18:02 +0800

    sdm660-common: Switch to xiaomi common libperfmgr
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Change-Id: If01e4e09f13d1c2e6010a64c679f3a7f2c3f543a

--- sdm660.mk
+++ sdm660.mk
@@ -333,8 +333,12 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_sdm660-libperfmgr \
+    android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Protobuf
 PRODUCT_PACKAGES += \
